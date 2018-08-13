fa935aaf51eb5635094ddf58c1778136dfe8bbd8
Phillip van Heerden <vanheerden.phillip@gmail.com>
2018-08-13 20:42:36 +0200

    Uncommented a test in TestAStarAgent

--- src/test/java/za/ac/sun/cs/search/singleagent/TestAStarAgent.java
+++ src/test/java/za/ac/sun/cs/search/singleagent/TestAStarAgent.java
@@ -7,11 +7,11 @@ public class TestAStarAgent {
     @Test
     public void smokeTest() {
         /* TODO Will probably be passed in as an argument. */
-        // short configuration[] = {8, 7, 5, 3, 0, 1, 4, 2, 6};
+        short configuration[] = {8, 7, 5, 3, 0, 1, 4, 2, 6};
         // short configuration[] = {1, 0, 2, 3, 4, 5, 6, 7, 8};
 
-        // AStarAgent aStarAgent = new AStarAgent(configuration);
+        AStarAgent aStarAgent = new AStarAgent(configuration);
 
-        // aStarAgent.solve();
+        aStarAgent.solve();
     }
 }
