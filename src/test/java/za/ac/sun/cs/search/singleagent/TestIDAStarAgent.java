90720d1759d7ac88c791e78ead88ae232fd55326
Zhunaid Mohamed <zhunaid@cs443392.sun.ac.za>
2018-08-15 11:15:28 +0200

    Swapped IDAStar tests

--- src/test/java/za/ac/sun/cs/search/singleagent/TestIDAStarAgent.java
+++ src/test/java/za/ac/sun/cs/search/singleagent/TestIDAStarAgent.java
@@ -1,32 +1,25 @@
 package za.ac.sun.cs.search.singleagent;
 
-import java.beans.Transient;
-
 import org.junit.Test;
-import za.ac.sun.cs.search.singleagent.Agent.IDAStarGridAgent;
+import za.ac.sun.cs.search.singleagent.Agent.IDAStarAgent;
 import za.ac.sun.cs.search.singleagent.Board.Direction;
-import za.ac.sun.cs.search.singleagent.Grid.ImplicitGrid;
-import za.ac.sun.cs.search.singleagent.Heuristic.EucledianHeuristic;
+import za.ac.sun.cs.search.singleagent.Board.ImplicitBoard;
+import za.ac.sun.cs.search.singleagent.Heuristic.MisplacedTilesHeuristic;
 
 public class TestIDAStarAgent {
 
     @Test
-
     public void smokeTest() throws Exception {
         /* TODO Will probably be passed in as an argument. */
 
-        boolean configuration[][] = { { false, true }, { true, true } };
-        boolean configuration2[][] = { { false, true }, { true, true } };
+        short configuration[] = { 8, 7, 5, 3, 0, 1, 4, 2, 6 };
+        short configuration2[] = { 8, 7, 5, 3, 0, 1, 4, 2, 6 };
 
-        short[] playerPosition = { 1, 0 };
-        short[] goalPosition = { 0, 1 };
-
-        IDAStarGridAgent idaStarAgent = new IDAStarGridAgent(configuration, playerPosition, goalPosition,
-                new EucledianHeuristic());
+        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new MisplacedTilesHeuristic());
         Direction[] path = idaStarAgent.solve();
 
-        ImplicitGrid grid = new ImplicitGrid(configuration2, playerPosition, goalPosition);
-        grid.visualizePath(path);
+        ImplicitBoard board = new ImplicitBoard(configuration2);
+        board.visualizePath(path);
 
     }
 }
