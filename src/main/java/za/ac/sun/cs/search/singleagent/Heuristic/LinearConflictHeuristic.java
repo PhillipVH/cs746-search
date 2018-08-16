9ea26c49d6aa60c75ccc95775a81ab7b9740abf9
Zhunaid Mohamed <zhunaid@cs443392.sun.ac.za>
2018-08-16 14:28:20 +0200

    Added LinearConflict Heuristic

---
+++ src/main/java/za/ac/sun/cs/search/singleagent/Heuristic/LinearConflictHeuristic.java
@@ -0,0 +1,110 @@
+package za.ac.sun.cs.search.singleagent.Heuristic;
+
+import za.ac.sun.cs.search.singleagent.Domain.Domain;
+
+public class LinearConflictHeuristic implements Heuristic {
+
+    @Override
+    public int getHeuristicCostEstimate(Domain domain) {
+        int cost = 0;
+        int idx = 0;
+        int N = domain.getSize();
+        short[] goalState = domain.getGoalState();
+
+        cost += linearVerticalConflict(domain, goalState);
+        cost += linearHorizontalConflict(domain, goalState);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                short tile = domain.getAt(i, j);
+                /* Don't count the blank tile. */
+                if (tile == 0) {
+                    idx++;
+                    continue;
+                }
+                if (tile != goalState[idx++]) {
+                    cost += getDistance(goalState, i, j, tile);
+                }
+            }
+        }
+
+        return cost;
+    }
+
+    private int getDistance(short[] goalState, int row, int column, short tile) {
+        int cost = 0;
+        int n = goalState.length;
+        int size = (int) Math.sqrt(n);
+
+        for (int i = 0; i < n; i++) {
+            if (goalState[i] == tile) {
+                cost += Math.abs((i / size) - row);
+                cost += Math.abs((i % size) - column);
+                return cost;
+            }
+        }
+
+        return 0;
+    }
+
+    private short[] getGoalTilePosition(short[] goalState, short tile, int size) {
+        short[] pos = new short[2];
+
+        for (int i = 0; i < goalState.length; i++) {
+            if (goalState[i] == tile) {
+                pos[0] = (short) (i / size);
+                pos[1] = (short) (i % size);
+                return pos;
+            }
+        }
+
+        return null;
+    }
+
+    private int linearVerticalConflict(Domain domain, short[] goalState) {
+        int cost = 0;
+        int n = goalState.length;
+        int size = (int) Math.sqrt(n);
+
+        for (int row = 0; row < size; row++) {
+            short max = -1;
+            for (int column = 0; column < size; column++) {
+                short tile = domain.getAt(row, column);
+
+                if (tile != 0 && getGoalTilePosition(goalState, tile, size)[1] == column) {
+                    if (tile > max) {
+                        max = tile;
+                    } else {
+                        cost += 2;
+                    }
+                }
+            }
+        }
+
+        return cost;
+    }
+
+    private int linearHorizontalConflict(Domain domain, short[] goalState) {
+        int cost = 0;
+        int n = goalState.length;
+        int size = (int) Math.sqrt(n);
+
+        for (int column = 0; column < size; column++) {
+            short max = -1;
+            for (int row = 0; row < size; row++) {
+                short tile = domain.getAt(row, column);
+
+                if (tile != 0 && getGoalTilePosition(goalState, tile, size)[0] == row) {
+                    if (tile > max) {
+                        max = tile;
+                    } else {
+                        cost += 2;
+                    }
+                }
+            }
+        }
+
+        return cost;
+    }
+
+}
--- src/test/java/za/ac/sun/cs/search/singleagent/TestKorf.java
+++ src/test/java/za/ac/sun/cs/search/singleagent/TestKorf.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import za.ac.sun.cs.search.singleagent.Agent.IDAStarAgent;
 import za.ac.sun.cs.search.singleagent.Board.Direction;
 import za.ac.sun.cs.search.singleagent.Heuristic.ManhattanHeuristic;
+import za.ac.sun.cs.search.singleagent.Heuristic.LinearConflictHeuristic;
 
 public class TestKorf {
 
@@ -14,7 +15,7 @@ public class TestKorf {
 
         short configuration[] = { 14, 13, 15, 7, 11, 12, 9, 5, 6, 0, 2, 1, 4, 8, 10, 3 };
 
-        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new ManhattanHeuristic());
+        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new LinearConflictHeuristic());
         Direction[] path = idaStarAgent.solve();
 
         Assert.assertEquals(276361933, idaStarAgent.getExploredNodes());
@@ -28,7 +29,7 @@ public class TestKorf {
 
         short configuration[] = { 13, 5, 4, 10, 9, 12, 8, 14, 2, 3, 7, 1, 0, 15, 11, 6 };
 
-        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new ManhattanHeuristic());
+        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new LinearConflictHeuristic());
         Direction[] path = idaStarAgent.solve();
 
         Assert.assertEquals(15300442, idaStarAgent.getExploredNodes());
@@ -42,7 +43,7 @@ public class TestKorf {
 
         short configuration[] = { 14, 7, 8, 2, 13, 11, 10, 4, 9, 12, 5, 0, 3, 6, 1, 15 };
 
-        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new ManhattanHeuristic());
+        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new LinearConflictHeuristic());
         Direction[] path = idaStarAgent.solve();
 
         Assert.assertEquals(565994203, idaStarAgent.getExploredNodes());
@@ -56,7 +57,7 @@ public class TestKorf {
 
         short configuration[] = { 3, 14, 9, 11, 5, 4, 8, 2, 13, 12, 6, 7, 10, 1, 15, 0 };
 
-        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new ManhattanHeuristic());
+        IDAStarAgent idaStarAgent = new IDAStarAgent(configuration, new LinearConflictHeuristic());
         Direction[] path = idaStarAgent.solve();
 
         Assert.assertEquals(1650696, idaStarAgent.getExploredNodes());
